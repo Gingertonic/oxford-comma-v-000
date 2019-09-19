0c16d2f948b35eec89ed63cdee6333961e3892e9
Beth Schofield <bethmschofield@gmail.com>
2019-09-19 18:15:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2 
-    last = array.pop
-    array << "and #{last}"
+    # last = array.pop
+    array << "and #{array.pop}"
     array.join(", ")
   else  
     array.join
