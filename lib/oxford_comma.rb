22b926f55d26089b60f1ce7ac659fa0aff1befed
Beth Schofield <bethmschofield@gmail.com>
2019-09-19 18:37:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,24 @@
 require 'pry'
 
-# def oxford_comma(array)
-#   if array.length == 2
-#     array.join(" and ")
-#   elsif array.length > 2 
-#     array.push("and #{array.pop}").join(", ")
-#   else  
-#     array.join
-#   end
-# end
-
-def oxford_comma(arr)
-  str = ""
-  arr.each.with_index do |word, idx|
-    if idx == arr.length - 1 && arr.length > 1
-      str << "and "
-    end 
-    str << word
-    str << ", " if arr.length >= 3 
+def oxford_comma(array)
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length > 2 
+    array.push("and #{array.pop}").join(", ")
+  else  
+    array.join
   end
-  # binding.pry
-  str
-end 
+end
+
+# def oxford_comma(arr)
+#   str = ""
+#   arr.each.with_index do |word, idx|
+#     if idx == arr.length - 1 && arr.length > 1
+#       str << "and "
+#     end 
+#     str << word
+#     str << ", " if arr.length >= 3 
+#   end
+#   # binding.pry
+#   str
+# end 
