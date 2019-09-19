34e174e236b44148779488e17e2397119bba7bef
Beth Schofield <bethmschofield@gmail.com>
2019-09-19 18:17:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length > 2 
-    array << "and #{array.pop}"
-    array.join(", ")
+    array.push("and #{array.pop}").join(", ")
   else  
     array.join
   end
