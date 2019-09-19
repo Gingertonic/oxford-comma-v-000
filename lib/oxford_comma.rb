da358f43cb17349fa6b14f79f8a251f3fefb9143
Beth Schofield <bethmschofield@gmail.com>
2019-09-19 18:13:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,8 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.length > 2 
   binding.pry
+    last = array.pop
+    array << "and starfruit"
     array.join(", ")
   else  
     array.join
