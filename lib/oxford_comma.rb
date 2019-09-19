d3abeb5300ca7351f6d0e51e6302778bbd4bb66f
Beth Schofield <bethmschofield@gmail.com>
2019-09-19 18:19:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 require 'pry'
 
-def oxford_comma(array)
-  if array.length == 2
-    array.join(" and ")
-  elsif array.length > 2 
-    array.push("and #{array.pop}").join(", ")
-  else  
-    array.join
-  end
-end
+# def oxford_comma(array)
+#   if array.length == 2
+#     array.join(" and ")
+#   elsif array.length > 2 
+#     array.push("and #{array.pop}").join(", ")
+#   else  
+#     array.join
+#   end
+# end
+
+def oxford_comma(arr)
+  
+end 
